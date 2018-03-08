c7efa1e34367b2d6abedb11f493d4dd4465310ae
Jennifer Chan <jsc058@ucsd.edu>
2018-03-07 17:50:19 -0800

    Fix now

--- pathfinder.cpp
+++ pathfinder.cpp
@@ -14,73 +14,77 @@
 #include <vector>
 #include "ActorGraph.hpp"
 #include <limits.h>
+# define INF 0x3f3f3f3f
 
 using namespace std;
 
 
 
-int main(int argc, const char ** argv) {
-        if (argc < 5) {
+int run(int argc, const char ** argv) {
+        if (argc < 4) {
                 cout << "Incorrect number of arguments." << endl;
                 exit(-1);
         }
 
         // Make the graph with the movie cast list
-        ActorGraph ag(argv[1], argv[2]);
+        ActorGraph ag(argv[0], argv[1]);
+        cout << "# nodes: " + ag.totalVertices << endl;
+        cout << "# movies: " + ag.totalMovies << endl;
+        cout << "# edges:" + ag.totalEdges << endl;
 
         // Initialize the file stream
-        ifstream infile(argv[3]);
+        ifstream infile(argv[2]);
         bool have_header = false;
+        string s;
 
-	while (infile) {
-		string s;
+        // get the next line
+        if (!getline( infile, s )) break;
 
-		// get the next line
-		if (!getline( infile, s )) break;
+        if (!have_header) {
+            // skip the header
+            have_header = true;
+            continue;
+        }
 
-		if (!have_header) {
-		    // skip the header
-		    have_header = true;
-		    continue;
-		}
+        istringstream ss( s );
+        vector <string> record;
 
-		istringstream ss( s );
-		vector <string> record;
+        while (ss) {
+            string next;
 
-		while (ss) {
-		    string next;
+            // get the next string before hitting a tab character and put it in 'next'
+            if (!getline( ss, next, '\t' )) break;
 
-		    // get the next string before hitting a tab character and put it in 'next'
-		    if (!getline( ss, next, '\t' )) break;
+            record.push_back( next );
+        }
 
-		    record.push_back( next );
-		}
+        if (record.size() != 2) {
+            // we should have exactly 2 columns
+            continue;
+        }
 
-		if (record.size() != 2) {
-		    // we should have exactly 2 columns
-		    continue;
-		}
+        string actor1(record[0]);
+        string actor2(record[1]);
+        bool success;
 
-		string actor1(record[0]);
-		string actor2(record[1]);
-		bool success;
+        if (**(argv[1]) == 'u') {
+                success = ag.UnweightedPath(actor1, actor2, argv[3]);
 
-		if (strcmp(argv[2],"u") == 0) {
-			success = ag.UnweightedPath(actor1, actor2, argv[4]);
+        } else {
+                success = ag.WeightedPath(actor1, actor2, argv[3]);
 
-		} else {
-			success = ag.WeightedPath(actor1, actor2, argv[4]);
-			cout << actor1 + actor2 << endl;
+        }
 
-		}
+        if (success == false) {
+                exit(-1);
+        }
 
-		auto it = ag.actors.begin();
-		for(; it != ag.actors.end(); ++it ) {
-			it->second.dist = INT_MAX;
-			it->second.prev = nullptr;
-			it->second.done = false;
-		}
-	}
+        auto it = ag.actors.begin();
+        for(; it != ag.actors.end(); ++it ) {
+                it->ActorNode.dist = INT_MAX;
+                it->prev = nullptr;
+                it->done = false;
+        }
 
 
-};
+}
